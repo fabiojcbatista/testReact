99622b5e1a94e5129d2d2433394e8337cbe4ca7e
fabiojcbatista <fabiojcbweb@gmail.com>
2022-01-08 21:59:49 -0300

    Test Link

--- src/test/unit/app.test.tsx
+++ src/test/unit/app.test.tsx
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react"
 import App from './../../App'
 
 
+test('render Link', ()=> {
+ render(< App/>);
+ const linkElement = screen.getByRole('link',{name: /learn react/i});
+ expect(linkElement).toBeInTheDocument();
+})
+
 test('render', ()=> {
  render(< App/>);
  const linkElement = screen.getByText(/learn react/i);
